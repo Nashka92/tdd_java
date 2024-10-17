c0150cd0e0f7039d0134c95ac8eb72db8146e214
Anna RAACH <annagraph92@gmail.com>
2024-10-17 11:29:36 +0200

    test(exo_4): tdd si le texte est => caracteres

--- src/test/java/rechercheville/RechercheVilleTest.java
+++ src/test/java/rechercheville/RechercheVilleTest.java
@@ -6,18 +6,34 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.example.rechercheville.NotFoundException;
 
+import java.util.List;
+
 public class RechercheVilleTest {
 
     private RechercheVille rechercheVille;
 
     @Test
     @DisplayName("Lever une exception si la recherche contient moins de 2 caractères")
-    public void whenSearchContainLessThan2letters(){
+    public void whenSearchContainLessThan2letters() {
         // arrange
         rechercheVille = new RechercheVille();
 
         // act and assert
-        Assert.assertThrows(NotFoundException.class, ()-> rechercheVille.rechercher("A"));
+        Assert.assertThrows(NotFoundException.class, () -> rechercheVille.rechercher("A"));
     }
 
+    @Test
+    @DisplayName("Si le texte est >= à 2 caractères, doit renvoyer tous les noms de ville commençant par le texte de recherche exact")
+    public void whenSearchTextIsGreaterThanOrEqualTo2Letters() throws NotFoundException {
+        // arrange
+        rechercheVille = new RechercheVille();
+
+        // act
+        List<String> result = rechercheVille.rechercher("Va");
+
+        // assert
+        Assert.assertEquals(2, result.size());
+        Assert.assertTrue(result.contains("Valence"));
+        Assert.assertTrue(result.contains("Vancouver"));
+    }
 }
