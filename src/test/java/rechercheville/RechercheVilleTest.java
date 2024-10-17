32570449391608654caccddc996a34aa38f43359
Anna RAACH <annagraph92@gmail.com>
2024-10-17 10:51:59 +0200

    test(exo_4): tdd when search contain less than 2 letters

---
+++ src/test/java/rechercheville/RechercheVilleTest.java
@@ -0,0 +1,22 @@
+package rechercheville;
+
+import org.example.rechercheville.RechercheVille;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class RechercheVilleTest {
+
+    private RechercheVille rechercheVille;
+
+    @Test
+    @DisplayName("Lever une exception si la recherche contient moins de 2 caractères")
+    public void whenSearchContainLessThan2letters(){
+        // arrange
+        rechercheVille = new RechercheVille();
+
+        // act and assert
+        Assert.assertThrows(ClassNotFoundException.class, ()-> rechercheVille.rechercher("A"));
+    }
+
+}
